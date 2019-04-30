145db28180345af79c314542674944d49a0211e5
Sophie Tagar <sophietagar@Sophies-MacBook-Pro.local>
2019-04-30 11:01:22 -0700

    first commit

--- teamcity_test.js
+++ teamcity_test.js
@@ -1,46 +1,38 @@
-var webdriver = require("selenium-webdriver");
-var Capabilities = webdriver.Capabilities;
-var Builder = webdriver.Builder;
-var By = webdriver.By;
+var Webdriver = require("selenium-webdriver");
+require("chromedriver");
+const { Eyes } = require("@applitools/eyes-selenium");
 
-var SeleniumSDK = require("eyes.selenium"); //Import Applitools SDK
-var Eyes = SeleniumSDK.Eyes;
+async function main() {
+  var eyes = new Eyes(); // Note 1
+  var apiKey = process.env.MY_APPLITOOLS_API_KEY;
+  eyes.setApiKey(apiKey); // Note 2
+  var innerDriver = new Webdriver.Builder() // Note 3
+    .withCapabilities(Webdriver.Capabilities.chrome())
+    .build();
 
-// Open a Chrome browser.
-var driver = new Builder().withCapabilities(Capabilities.chrome()).build();
-
-// Initialize the eyes SDK and set your private API key.
-var eyes = new Eyes();
-eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
-
-//scroll the entire page
-eyes.setForceFullPageScreenshot(true);
-
-try {
   eyes.setBatch(
     process.env.APPLITOOLS_BATCH_NAME,
     process.env.APPLITOOLS_BATCH_ID
   );
-  // Start the test and set the g's viewport size to 600 x 800
-  eyes.open(driver, "Test app", "Test TeamCity", { width: 600, height: 800 });
-
-  // Navigate the browser to the demo app
-  driver.get("https://demo.applitools.com");
-
-  // Visual checkpoint #1.
-  eyes.checkWindow("Login Window test");
-  //🌟 Note: You can have multiple "checkWindow" to create multiple test steps within a test.🌟
-  //For example, you may want to test errors in the login window after clicking a login button,
-  //In that case, you may add the following before you call eyes.close().
-  //This will create a test with two test steps.
-  //driver.click("login");
-  //eyes.checkWindow("Login Window Error");
-  // End the test.
-  eyes.close(false);
-} finally {
-  // Close the browser.
-  driver.quit();
-
-  // If the test was aborted before eyes.close was called ends the test as aborted.
-  eyes.abortIfNotClosed();
+  /*
+   * Add optional global setup/defaults here                            // Note 4
+   */
+  var viewportSize = { width: 1024, height: 768 };
+  driver = await eyes.open(
+    innerDriver, // Note 5                // Note 5
+    "My Application Name",
+    "TeamCity",
+    viewportSize
+  );
+  //.then(function (driver){ afterOpen(eyes, driver)});
+  try {
+    var website = "https://applitools.com/helloworld";
+    driver.get(website); // Note 6
+    await eyes.checkWindow("initial screen"); // Note 7
+    let testResults = await eyes.close(false); // Note 8                         // Note 8
+  } finally {
+    eyes.abortIfNotClosed(); // Note 9
+  }
+  innerDriver.quit(); // Close the browser.                                  // Note 10
 }
+main();
